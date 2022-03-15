ce15043881994d47f7bdb949308ae4aa14e4c728
Sulayman-Hafizullah <38872624+Sulayman-Hafizullah@users.noreply.github.com>
2022-03-15 15:16:18 -0500

    Update Cube.java

--- Java-Snake-Game/src/com/snake/Cube.java
+++ Java-Snake-Game/src/com/snake/Cube.java
@@ -1,5 +1,8 @@
 package com.snake;
 
+import java.awt.Color;
+import java.lang.Object;
+import javax.media.j3d.*;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -7,47 +10,77 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import com.sun.j3d.utils.geometry.Sphere;
+
 public class Cube {
-    int x, y, size, shift;
-    Point[] cubeOnePoints;
-    Point[] cubeTwoPoints;
-    public Cube(int x, int y, int size, int shift) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.shift = shift;
-        cubeOnePoints = getCubeOnePoints();
-        cubeTwoPoints = getCubeTwoPoints();
-    }
+	int x, y, size, shift;
+	Point[] cubeOnePoints;
+	Point[] cubeTwoPoints;
+	int[] xVals = new int[4];
+	int[] yVals = new int[4];
 
-    private Point[] getCubeOnePoints() {
-        Point[] points = new Point[4];
-        points[0] = new Point(x, y);
-        points[1] = new Point(x + size, y);
-        points[2] = new Point(x + size, y + size);
-        points[3] = new Point(x, y + size);
-        return points;
-    }
+	public Cube(int x, int y, int size, int shift) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.shift = shift;
+		cubeOnePoints = getCubeOnePoints();
+		cubeTwoPoints = getCubeTwoPoints();
+	}
 
-    private Point[] getCubeTwoPoints() {
-        int newX = x + shift;
-        int newY = y + shift;
-        Point[] points = new Point[4];
-        points[0] = new Point(newX, newY);
-        points[1] = new Point(newX + size, newY);
-        points[2] = new Point(newX + size, newY + size);
-        points[3] = new Point(newX, newY + size);
-        return points;
-    }
+	private Point[] getCubeOnePoints() {
+		Point[] points = new Point[4];
+		points[0] = new Point(x, y);
+		points[1] = new Point(x + size, y);
+		points[2] = new Point(x + size, y + size);
+		points[3] = new Point(x, y + size);
+		return points;
+	}
 
-    public void drawCube(Graphics g) {
-        g.drawRect(x, y, size, size);
-        g.drawRect(x + shift, y + shift, size, size);
-        // draw connecting lines
-        for (int i = 0; i < 4; i++) {
-            g.drawLine(cubeOnePoints[i].x, cubeOnePoints[i].y, 
-                    cubeTwoPoints[i].x, cubeTwoPoints[i].y);
-        }
-    }
+	private Point[] getCubeTwoPoints() {
+		int newX = x + shift;
+		int newY = y + shift;
+		Point[] points = new Point[4];
+		points[0] = new Point(newX, newY);
+		points[1] = new Point(newX + size, newY);
+		points[2] = new Point(newX + size, newY + size);
+		points[3] = new Point(newX, newY + size);
+		return points;
+	}
+
+	public void drawCube(Graphics g) {
+		g.drawRect(x, y, size, size);
+		// g.setColor(Color.GRAY);
+		// left side of cube
+		xVals[0] = cubeOnePoints[0].x;
+		xVals[1] = cubeOnePoints[3].x;
+		yVals[0] = cubeOnePoints[0].y;
+		yVals[1] = cubeOnePoints[3].y;
+		xVals[2] = cubeTwoPoints[3].x;
+		xVals[3] = cubeTwoPoints[0].x;
+		yVals[2] = cubeTwoPoints[3].y;
+		yVals[3] = cubeTwoPoints[0].y;
+		g.fillPolygon(xVals, yVals, 4);
+
+		// top of cube
+		// g.setColor(Color.MAGENTA);
+		xVals[0] = cubeOnePoints[0].x;
+		xVals[1] = cubeOnePoints[1].x;
+		xVals[2] = cubeTwoPoints[1].x;
+		xVals[3] = cubeTwoPoints[0].x;
+		yVals[0] = cubeOnePoints[0].y;
+		yVals[1] = cubeOnePoints[1].y;
+		yVals[2] = cubeTwoPoints[1].y;
+		yVals[3] = cubeTwoPoints[0].y;
+		g.fillPolygon(xVals, yVals, 4);
+		// g.drawRect(x + shift, y + shift, size, size);
+		g.fillRect(x + shift, y + shift, size, size);
+		// draw connecting lines
+		for (int i = 0; i < 4; i++) {
+			g.drawLine(cubeOnePoints[i].x, cubeOnePoints[i].y, cubeTwoPoints[i].x, cubeTwoPoints[i].y);
+
+		}
+
+	}
+
 }
-
