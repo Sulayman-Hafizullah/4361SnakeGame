c81003e6135a729b3e12d7566fc87d128f63898a
Sulayman-Hafizullah <38872624+Sulayman-Hafizullah@users.noreply.github.com>
2022-04-16 16:04:47 -0500

    Add files via upload

--- Java-Snake-Game/src/com/snake/Board.java
+++ Java-Snake-Game/src/com/snake/Board.java
@@ -33,7 +33,7 @@ public class Board extends JPanel implements ActionListener {
 	private final int DOT_SIZE = 10;
 	private final int ALL_DOTS = 900;
 	private final int RAND_POS = 29;
-	private final int TARGET = 4;
+	private final int TARGET = 15;
 	private final int CUBE_DEPTH = 5; // parameter for 3d cube
 
 	private final int x[] = new int[ALL_DOTS];
